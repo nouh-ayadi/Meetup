46c53dfff6db08e2bbafc86f398884f9ad8bc94d
Nouh <nouh@MacBook-Air-ONprint.local>
2017-11-08 09:33:14 +0100

    Page d'inscription et connection avec firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import './stylus/main.styl'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import store from './store'
 import DateFilter from './filters/date'
@@ -17,5 +18,14 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  created() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAqciI1dQTlPw_UcOc4GZaWcXJsrpEjcWA',
+      authDomain: 'siteweb-37a0b.firebaseapp.com',
+      databaseURL: 'https://siteweb-37a0b.firebaseio.com',
+      projectId: 'siteweb-37a0b',
+      storageBucket: 'siteweb-37a0b.appspot.com'
+    })
+  }
 })
